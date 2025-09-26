96079b61152a92d629e53b018e256cd3d4edfa52
Naser Mirzaei <nasermirzaei89@gmail.com>
2025-09-26 19:28:24 -0400

    Add more buttons to editor

--- web/assets/scripts.js
+++ web/assets/scripts.js
@@ -21,6 +21,8 @@ import Strike from '@tiptap/extension-strike'
 import Dropcursor from '@tiptap/extension-dropcursor'
 import Gapcursor from '@tiptap/extension-gapcursor'
 import { UndoRedo } from '@tiptap/extensions'
+import Image from '@tiptap/extension-image'
+import Link from '@tiptap/extension-link'
 
 const initWysiwygEditor = (element) => {
     const editorContainer = document.createElement("div");
@@ -66,6 +68,14 @@ const initWysiwygEditor = (element) => {
             ListItem,
 
             Blockquote,
+
+            Link.configure({
+                openOnClick: false,
+            }),
+            Image.configure({
+                inline: true,
+                allowBase64: true,
+            }),
         ],
         content: element.value,
         editorProps: {
@@ -73,7 +83,7 @@ const initWysiwygEditor = (element) => {
                 class: "wysiwyg-editor-content"
             },
         },
-        onUpdate({editor}) {
+        onUpdate({ editor }) {
             element.value = editor.getHTML();
         },
     });
@@ -83,53 +93,59 @@ const initWysiwygEditor = (element) => {
         [
             {
                 title: "Heading 2",
-                content: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>format-header-2</title><path d="M3,4H5V10H9V4H11V18H9V12H5V18H3V4M21,18H15A2,2 0 0,1 13,16C13,15.47 13.2,15 13.54,14.64L18.41,9.41C18.78,9.05 19,8.55 19,8A2,2 0 0,0 17,6A2,2 0 0,0 15,8H13A4,4 0 0,1 17,4A4,4 0 0,1 21,8C21,9.1 20.55,10.1 19.83,10.83L15,16H21V18Z" /></svg>`,
-                onClick: () => editor.chain().focus().toggleHeading({level: 2}).run(),
+                content: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M3 4h2v6h4V4h2v14H9v-6H5v6H3zm18 14h-6a2 2 0 0 1-2-2c0-.53.2-1 .54-1.36l4.87-5.23c.37-.36.59-.86.59-1.41a2 2 0 0 0-2-2a2 2 0 0 0-2 2h-2a4 4 0 0 1 4-4a4 4 0 0 1 4 4c0 1.1-.45 2.1-1.17 2.83L15 16h6z"/></svg>`,
+                onClick: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
                 isActive: (editor) => editor.isActive('heading', { level: 2 })
             },
             {
                 title: "Heading 3",
-                content: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>format-header-3</title><path d="M3,4H5V10H9V4H11V18H9V12H5V18H3V4M15,4H19A2,2 0 0,1 21,6V16A2,2 0 0,1 19,18H15A2,2 0 0,1 13,16V15H15V16H19V12H15V10H19V6H15V7H13V6A2,2 0 0,1 15,4Z" /></svg>`,
-                onClick: () => editor.chain().focus().toggleHeading({level: 3}).run(),
+                content: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M3 4h2v6h4V4h2v14H9v-6H5v6H3zm12 0h4a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2v-1h2v1h4v-4h-4v-2h4V6h-4v1h-2V6a2 2 0 0 1 2-2"/></svg>`,
+                onClick: () => editor.chain().focus().toggleHeading({ level: 3 }).run(),
                 isActive: (editor) => editor.isActive('heading', { level: 3 })
             },
         ],
         [
             {
                 title: "Bold",
-                content: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>format-bold</title><path d="M13.5,15.5H10V12.5H13.5A1.5,1.5 0 0,1 15,14A1.5,1.5 0 0,1 13.5,15.5M10,6.5H13A1.5,1.5 0 0,1 14.5,8A1.5,1.5 0 0,1 13,9.5H10M15.6,10.79C16.57,10.11 17.25,9 17.25,8C17.25,5.74 15.5,4 13.25,4H7V18H14.04C16.14,18 17.75,16.3 17.75,14.21C17.75,12.69 16.89,11.39 15.6,10.79Z" /></svg>`,
+                content: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M13.5 15.5H10v-3h3.5A1.5 1.5 0 0 1 15 14a1.5 1.5 0 0 1-1.5 1.5m-3.5-9h3A1.5 1.5 0 0 1 14.5 8A1.5 1.5 0 0 1 13 9.5h-3m5.6 1.29c.97-.68 1.65-1.79 1.65-2.79c0-2.26-1.75-4-4-4H7v14h7.04c2.1 0 3.71-1.7 3.71-3.79c0-1.52-.86-2.82-2.15-3.42"/></svg>`,
                 onClick: () => editor.chain().focus().toggleBold().run(),
                 isActive: (editor) => editor.isActive('bold')
             },
             {
                 title: "Italic",
-                content: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>format-italic</title><path d="M10,4V7H12.21L8.79,15H6V18H14V15H11.79L15.21,7H18V4H10Z" /></svg>`,
+                content: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M10 4v3h2.21l-3.42 8H6v3h8v-3h-2.21l3.42-8H18V4z"/></svg>`,
                 onClick: () => editor.chain().focus().toggleItalic().run(),
                 isActive: (editor) => editor.isActive('italic')
             },
             {
                 title: "Underline",
-                content: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>format-underline</title><path d="M5,21H19V19H5V21M12,17A6,6 0 0,0 18,11V3H15.5V11A3.5,3.5 0 0,1 12,14.5A3.5,3.5 0 0,1 8.5,11V3H6V11A6,6 0 0,0 12,17Z" /></svg>`,
+                content: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M5 21h14v-2H5zm7-4a6 6 0 0 0 6-6V3h-2.5v8a3.5 3.5 0 0 1-3.5 3.5A3.5 3.5 0 0 1 8.5 11V3H6v8a6 6 0 0 0 6 6"/></svg>`,
                 onClick: () => editor.chain().focus().toggleUnderline().run(),
                 isActive: (editor) => editor.isActive('underline')
             },
             {
                 title: "Strikethrough",
-                content: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>format-strikethrough-variant</title><path d="M7.2 9.8C6 7.5 7.7 4.8 10.1 4.3C13.2 3.3 17.7 4.7 17.6 8.5H14.6C14.6 8.2 14.5 7.9 14.5 7.7C14.3 7.1 13.9 6.8 13.3 6.6C12.5 6.3 11.2 6.4 10.5 6.9C9 8.2 10.4 9.5 12 10H7.4C7.3 9.9 7.3 9.8 7.2 9.8M21 13V11H3V13H12.6C12.8 13.1 13 13.1 13.2 13.2C13.8 13.5 14.3 13.7 14.5 14.3C14.6 14.7 14.7 15.2 14.5 15.6C14.3 16.1 13.9 16.3 13.4 16.5C11.6 17 9.4 16.3 9.5 14.1H6.5C6.4 16.7 8.6 18.5 11 18.8C14.8 19.6 19.3 17.2 17.3 12.9L21 13Z" /></svg>`,
+                content: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M7.2 9.8c-1.2-2.3.5-5 2.9-5.5c3.1-1 7.6.4 7.5 4.2h-3c0-.3-.1-.6-.1-.8c-.2-.6-.6-.9-1.2-1.1c-.8-.3-2.1-.2-2.8.3C9 8.2 10.4 9.5 12 10H7.4c-.1-.1-.1-.2-.2-.2M21 13v-2H3v2h9.6c.2.1.4.1.6.2c.6.3 1.1.5 1.3 1.1c.1.4.2.9 0 1.3c-.2.5-.6.7-1.1.9c-1.8.5-4-.2-3.9-2.4h-3c-.1 2.6 2.1 4.4 4.5 4.7c3.8.8 8.3-1.6 6.3-5.9z"/></svg>`,
                 onClick: () => editor.chain().focus().toggleStrike().run(),
                 isActive: (editor) => editor.isActive('strike')
+            },
+            {
+                title: "Code",
+                content: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m14.6 16.6l4.6-4.6l-4.6-4.6L16 6l6 6l-6 6zm-5.2 0L4.8 12l4.6-4.6L8 6l-6 6l6 6z"/></svg>`,
+                onClick: () => editor.chain().focus().toggleCode().run(),
+                isActive: (editor) => editor.isActive('code')
             },
         ],
         [
             {
                 title: "Bulleted List",
-                content: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>format-list-bulleted</title><path d="M7,5H21V7H7V5M7,13V11H21V13H7M4,4.5A1.5,1.5 0 0,1 5.5,6A1.5,1.5 0 0,1 4,7.5A1.5,1.5 0 0,1 2.5,6A1.5,1.5 0 0,1 4,4.5M4,10.5A1.5,1.5 0 0,1 5.5,12A1.5,1.5 0 0,1 4,13.5A1.5,1.5 0 0,1 2.5,12A1.5,1.5 0 0,1 4,10.5M7,19V17H21V19H7M4,16.5A1.5,1.5 0 0,1 5.5,18A1.5,1.5 0 0,1 4,19.5A1.5,1.5 0 0,1 2.5,18A1.5,1.5 0 0,1 4,16.5Z" /></svg>`,
+                content: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M7 5h14v2H7zm0 8v-2h14v2zM4 4.5A1.5 1.5 0 0 1 5.5 6A1.5 1.5 0 0 1 4 7.5A1.5 1.5 0 0 1 2.5 6A1.5 1.5 0 0 1 4 4.5m0 6A1.5 1.5 0 0 1 5.5 12A1.5 1.5 0 0 1 4 13.5A1.5 1.5 0 0 1 2.5 12A1.5 1.5 0 0 1 4 10.5M7 19v-2h14v2zm-3-2.5A1.5 1.5 0 0 1 5.5 18A1.5 1.5 0 0 1 4 19.5A1.5 1.5 0 0 1 2.5 18A1.5 1.5 0 0 1 4 16.5"/></svg>`,
                 onClick: () => editor.chain().focus().toggleBulletList().run(),
                 isActive: (editor) => editor.isActive('bulletList')
             },
             {
                 title: "Numbered List",
-                content: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>format-list-numbered</title><path d="M7,13V11H21V13H7M7,19V17H21V19H7M7,7V5H21V7H7M3,8V5H2V4H4V8H3M2,17V16H5V20H2V19H4V18.5H3V17.5H4V17H2M4.25,10A0.75,0.75 0 0,1 5,10.75C5,10.95 4.92,11.14 4.79,11.27L3.12,13H5V14H2V13.08L4,11H2V10H4.25Z" /></svg>`,
+                content: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M7 13v-2h14v2zm0 6v-2h14v2zM7 7V5h14v2zM3 8V5H2V4h2v4zm-1 9v-1h3v4H2v-1h2v-.5H3v-1h1V17zm2.25-7a.75.75 0 0 1 .75.75c0 .2-.08.39-.21.52L3.12 13H5v1H2v-.92L4 11H2v-1z"/></svg>`,
                 onClick: () => editor.chain().focus().toggleOrderedList().run(),
                 isActive: (editor) => editor.isActive('orderedList')
             },
@@ -137,16 +153,49 @@ const initWysiwygEditor = (element) => {
         [
             {
                 title: "Blockquote",
-                content: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>format-quote-open</title><path d="M10,7L8,11H11V17H5V11L7,7H10M18,7L16,11H19V17H13V11L15,7H18Z" /></svg>`,
+                content: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m10 7l-2 4h3v6H5v-6l2-4zm8 0l-2 4h3v6h-6v-6l2-4z"/></svg>`,
                 onClick: () => editor.chain().focus().toggleBlockquote().run(),
                 isActive: (editor) => editor.isActive('blockquote')
+            },
+        ],
+        [
+            {
+                title: "Link",
+                content: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7a5 5 0 0 0-5 5a5 5 0 0 0 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1M8 13h8v-2H8zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4a5 5 0 0 0 5-5a5 5 0 0 0-5-5"/></svg>`,
+                onClick: () => {
+                    const previousUrl = editor.getAttributes('link').href
+                    const url = window.prompt('URL', previousUrl || 'https://')
+                    // cancelled
+                    if (url === null) {
+                        return
+                    }
+                    // empty
+                    if (url === '') {
+                        editor.chain().focus().extendMarkRange('link').unsetLink().run()
+                        return
+                    }
+                    // update link
+                    editor.chain().focus().extendMarkRange('link').setLink({ href: url }).run()
+                },
+                isActive: (editor) => editor.isActive('link')
+            },
+            {
+                title: "Image",
+                content: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="m8.5 13.5l2.5 3l3.5-4.5l4.5 6H5m16 1V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2"/></svg>`,
+                onClick: () => {
+                    const url = window.prompt('Image URL', editor.getAttributes('image').src || 'https://')
+                    if (url) {
+                        editor.chain().focus().setImage({ src: url }).run()
+                    }
+                },
+                isActive: (editor) => editor.isActive('image')
             },
         ],
     ]
 
     // Create toolbar buttons with active state tracking
     const toolbarButtons = new Map(); // Store button references for active state updates
-    
+
     toolbar.forEach((toolset) => {
         const div = document.createElement("div");
         div.classList.add("toolset");
@@ -157,12 +206,12 @@ const initWysiwygEditor = (element) => {
             button.type = "button";
             button.title = tool.title;
             button.onclick = tool.onClick;
-            
+
             // Store button reference with its check function
             if (tool.isActive) {
                 toolbarButtons.set(button, tool.isActive);
             }
-            
+
             div.appendChild(button);
         });
 
